932cd8897aee26bcf6dfaebc6210754fa5631013
TianshuWang <wangtianshu17@gmail.com>
2024-09-03 14:34:32 -0300

    add goroutine

--- cpu_bench/cpu.go
+++ cpu_bench/cpu.go
@@ -27,26 +27,26 @@ func sendData(ch chan<- bool) {
 	for i := 0; i < 10; i++ {
 		ch <- true
 	}
-	close(ch)
+	close(ch) // when close the channel, the channel will not be able to receive any more data, but still permit the consumption of the data
 }
 
 func receiveData(wg *sync.WaitGroup, threadId int, ch <-chan bool) {
-	//defer wg.Done()
+	defer wg.Done() // make sure that this thread will be finished after the execution of this function
 	for data := range ch {
 		bcrypt.GenerateFromPassword([]byte("test"), 10)
 		fmt.Printf("Thread: %d\n", threadId)
 		fmt.Println("Data Received:", data)
 	}
-	wg.Done()
+	//wg.Done()
 }
 
 func limitParallel(n int) {
 	ch := make(chan bool)
 	go sendData(ch)
 	var wg sync.WaitGroup
-	//wg.Add(n)
+	//wg.Add(n) when already knows how many threads will be running
 	for i := 0; i < n; i++ {
-		wg.Add(1)
+		wg.Add(1) //when don't know how many threads will be run
 		go receiveData(&wg, i, ch)
 	}
 
